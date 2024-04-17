c0b6b79cb33a44ad351b4f925fe9d1ce49484b5b
chithien12322 <90127844+chithien12322@users.noreply.github.com>
2024-04-17 14:28:06 +0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 module.exports = {
-    TOKEN: process.env.TOKEN || "MTIyOTc5NzQzNTgwMzQzOTE4Nw.G2tAfC.oFCfcswT4islB0vYGbpYNYGGdC6qVF_xDHJ79c", 
+    TOKEN: process.env.TOKEN || "MTIyOTc5NzQzNTgwMzQzOTE4Nw.GxSL1C.klnJrztKz1UFq2K0h2STxnASnK21x4XfU4rEvY", 
     PREFIX: process.env.PREFIX || "d", 
     OWNER_ID: process.env.OWNER_ID || "571542657378156545", 
     EMPTY_LEAVE: 60, 
